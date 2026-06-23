87b67249de3491da220fbf353bcb51766fd25ddb
aahassofttech <aahassofttech@gmail.com>
2026-06-23 17:30:49 +0530

    Update route.ts

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import { NextRequest, NextResponse } from "next/server";
 import { getPostById, updatePost, deletePost, toggleStatus } from "@/lib/posts";
 import { BlogPostInput } from "@/types/blog";
